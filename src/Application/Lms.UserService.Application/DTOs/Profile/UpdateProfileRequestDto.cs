7d309f9fa12c239bdaef7bcaed8cfcb3170391aa
Ayler Sabbagh <ayler.sabbagh@gmail.com>
2026-05-22 19:37:41 +0200

    Added profile validation

--- src/Application/Lms.UserService.Application/DTOs/Profile/UpdateProfileRequestDto.cs
+++ src/Application/Lms.UserService.Application/DTOs/Profile/UpdateProfileRequestDto.cs
@@ -1,14 +1,23 @@
-﻿namespace Lms.UserService.Application.DTOs.Profile;
+﻿using System.ComponentModel.DataAnnotations;
 
-//datan som frontend skickar in när profilen uppdateras
+namespace Lms.UserService.Application.DTOs.Profile;
+
+//datan som frontend skickar in när profilen uppdateras så att asp.net kan kontrollera att:
+//namn, efternamn, maxlängd och telenfonformat fnns innan den når servicen
 public class UpdateProfileRequestDto
 {
+    [Required]
+    [MaxLength(50)]
     public string FirstName { get; set; } = null!;
 
+    [Required]
+    [MaxLength(50)]
     public string LastName { get; set; } = null!;
 
+    [Phone]
     public string? PhoneNumber { get; set; }
 
+    [MaxLength(500)]
     public string? Bio { get; set; }
 
     public string? ProfileImageUrl { get; set; }
