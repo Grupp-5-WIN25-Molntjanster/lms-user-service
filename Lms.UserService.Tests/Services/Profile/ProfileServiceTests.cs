41978e4962ff8a8c45a9de25ee473c3b53c46231
Ayler <ayler.sabbagh@gmail.com>
2026-05-24 20:01:47 +0200

    Added unit test for profile service

---
+++ Lms.UserService.Tests/Services/Profile/ProfileServiceTests.cs
@@ -0,0 +1,45 @@
+﻿using Lms.UserService.Infrastructure.Services.Profile;
+using Lms.UserService.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Lms.UserService.Domain.Entities;
+
+namespace Lms.UserService.Tests.Services.Profile;
+
+public class ProfileServiceTests
+{
+    [Fact]
+    public async Task GetProfileAsync_Returns_Profile()
+    {
+        //skapar fake databas i minnet
+        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            .UseInMemoryDatabase(databaseName: "TestDb")
+            .Options;
+
+        //skapar dbcontext
+        var context = new ApplicationDbContext(options);
+
+        //lägger in testprofil i databasen
+        context.Profiles.Add(new ProfileEntity
+        {
+            UserId = "user-123",
+            FirstName = "Ayler",
+            LastName = "Sabbagh",
+            Bio = "Test bio"
+        });
+
+        await context.SaveChangesAsync();
+
+        //skapar service
+        var service = new ProfileService(context);
+
+        //hämtar profil
+        var result = await service.GetProfileAsync("user-123");
+
+        //kontrollerar att rätt data returneras
+        Assert.NotNull(result);
+
+        Assert.Equal("Ayler", result.FirstName);
+        Assert.Equal("Sabbagh", result.LastName);
+        Assert.Equal("Test bio", result.Bio);
+    }
+}
