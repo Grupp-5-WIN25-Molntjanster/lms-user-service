7ba896a93221e876355572d9810d38c190f4b3d9
Ayler <ayler.sabbagh@gmail.com>
2026-05-27 18:25:59 +0200

    feat(profile): add skills controller

---
+++ src/Web/Lms.UserService.Web/Controllers/Skills/SkillsController.cs
@@ -0,0 +1,60 @@
+﻿using Lms.UserService.Application.DTOs.Skills;
+using Lms.UserService.Application.Interfaces.Skills;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Lms.UserService.Web.Controllers.Skills;
+
+[ApiController]
+[Route("api/profile/skills")]
+[Authorize]
+
+public class SkillsController : ControllerBase
+{
+    private readonly ISkillService _skillService;
+
+    public SkillsController(ISkillService skillService)
+    {
+        _skillService = skillService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetSkills()
+    {
+        var userId = User.FindFirstValue("sub");
+
+        if (userId == null)
+            return Unauthorized();
+
+        var skills = await _skillService.GetSkillsAsync(userId);
+
+        return Ok(skills);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateSkill(CreateSkillRequestDto request)
+    {
+        var userId = User.FindFirstValue("sub");
+
+        if (userId == null)
+            return Unauthorized();
+
+        var skill = await _skillService.CreateSkillAsync(userId, request);
+
+        return Ok(skill);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteSkill(Guid id)
+    {
+        var userId = User.FindFirstValue("sub");
+
+        if (userId == null)
+            return Unauthorized();
+
+        await _skillService.DeleteSkillAsync(id, userId);
+
+        return NoContent();
+    }
+}
