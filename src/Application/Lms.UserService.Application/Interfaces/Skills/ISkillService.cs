262202b2ab9bb1c9db2edbf389204feeb7566faa
Ayler <ayler.sabbagh@gmail.com>
2026-05-27 18:23:12 +0200

    feat(profile): add skills interface

---
+++ src/Application/Lms.UserService.Application/Interfaces/Skills/ISkillService.cs
@@ -0,0 +1,12 @@
+﻿using Lms.UserService.Application.DTOs.Skills;
+
+namespace Lms.UserService.Application.Interfaces.Skills;
+
+public interface ISkillService
+{
+    Task<List<SkillResponseDto>> GetSkillsAsync(string userId);
+
+    Task<SkillResponseDto> CreateSkillAsync(string userId, CreateSkillRequestDto request);
+
+    Task DeleteSkillAsync(Guid id, string userId);
+}
