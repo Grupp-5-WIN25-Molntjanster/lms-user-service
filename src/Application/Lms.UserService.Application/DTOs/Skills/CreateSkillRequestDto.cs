8a7f4eea6df8c1d130eda2b9b15a152a430fd71b
Ayler <ayler.sabbagh@gmail.com>
2026-05-27 18:20:20 +0200

    feat(profile): add skill dtos

---
+++ src/Application/Lms.UserService.Application/DTOs/Skills/CreateSkillRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lms.UserService.Application.DTOs.Skills;
+
+public class CreateSkillRequestDto
+{
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; } = null!;
+}
---
+++ src/Application/Lms.UserService.Application/DTOs/Skills/SkillResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Lms.UserService.Application.DTOs.Skills;
+
+public class SkillResponseDto
+{
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
